d65bbb2f15c9e6cdc4aa29cf666854914a457290
khengeian <33573486+khengeian@users.noreply.github.com>
2017-11-14 13:47:29 +0800

    Delete build.js

--- build.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello Mr. Hassan, this is build engineering environment.The tools that can be use are git,bracket as editor');
-});
-
-app.listen(process.env.PORT || 3000);
